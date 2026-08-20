d56536d290001c75de3295bc6ee4df0491873200
hrzndelta <hrzndelta@proton.me>
2026-08-20 21:10:31 +0800

    Adding sepolicies to config.mk and removed relaxed library check

--- config.mk
+++ config.mk
@@ -1,11 +1,22 @@
-BUILD_PATH := packages/apps/ViPER4AndroidFX
+VIPER4ANDROIDFX_PATH := vendor/ViPER4AndroidFX
+
+BUILD_PATH := $(VIPER4ANDROIDFX_PATH)
 
 PRODUCT_SOONG_NAMESPACES += \
-    packages/apps/ViPER4AndroidFX
+   $(VIPER4ANDROIDFX_PATH)
 
 PRODUCT_PACKAGES += \
     ViPER4AndroidFX \
-    libv4a_re
+    libv4a_aidl
 
-# Time to relax
-RELAX_USES_LIBRARY_CHECK := true
+# SEPolicy
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral redfin oriole raven panther cheetah lynx felix shiba husky akita tokay caiman komodo tegu frankel blazer mustang rango stallion, $(LINEAGE_BUILD)),)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+   $(VIPER4ANDROIDFX_PATH)/sepolicy/audioserver_viper4android_pixel.te
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+   $(VIPER4ANDROIDFX_PATH)/sepolicy/audioserver_viper4android.te
+endif
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
